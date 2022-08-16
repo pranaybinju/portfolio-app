a3a8ef723ad1d4ed7d6ddbdae3964316dcecdf0f
PKB2810 <pranay@kiprosh.com>
2022-08-16 21:10:50 +0530

    prettifying components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,19 @@
-import Info from "./Info";
-import Skills from "./Skills";
-import Achievements from "./Achievements";
-import Contact from "./Contact";
+import Info from "./Info"
+import Skills from "./Skills"
+import Achievements from "./Achievements"
+import Contact from "./Contact"
 
 const Main = () => {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <Info />
-      <div style={{ display: "flex" }}>
-        <Skills />
-        <Achievements />
-        <Contact />
-      </div>
-    </div>
-  );
-};
+	return (
+		<div style={{ padding: "2rem" }}>
+			<Info />
+			<div style={{ display: "flex" }}>
+				<Skills />
+				<Achievements />
+				<Contact />
+			</div>
+		</div>
+	)
+}
 
-export default Main;
+export default Main
--- src/tests/Info.test.js
+++ src/tests/Info.test.js
@@ -1,12 +1,12 @@
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { render } from "@testing-library/react"
+import "@testing-library/jest-dom"
 
-import Info from "../components/Info";
+import Info from "../components/Info"
 
 test("render Info", () => {
-  console.log("hi");
-  const { getByText } = render(<Info />);
-  expect(
-    getByText("Pokemon Champion and Trainer from Japan")
-  ).toBeInTheDocument();
-});
+	console.log("hi")
+	const { getByText } = render(<Info />)
+	expect(
+		getByText("Pokemon Champion and Trainer from Japan")
+	).toBeInTheDocument()
+})
