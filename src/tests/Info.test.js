e743a9a394397a32800469dd019d465241e80b5e
PKB2810 <pranay@kiprosh.com>
2022-08-16 21:11:17 +0530

    prettifying component

--- src/tests/Info.test.js
+++ src/tests/Info.test.js
@@ -8,5 +8,5 @@ test("render Info", () => {
 	const { getByText } = render(<Info />)
 	expect(
 		getByText("Pokemon Champion and Trainer from Japan")
-	).toBeInTheDocument()
+	).toBeInTheDocument();
 })
